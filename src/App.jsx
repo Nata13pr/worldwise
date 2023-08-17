b8bcccd36f2a3022d413cead71dc8037dfb89a04
Nataliia <Nata13pr@gmail.com>
2023-08-17 18:34:00 +0300

    added css modules,implemented countries list

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,55 @@
-import { BrowserRouter, Routes,Route } from "react-router-dom";
-import Homepage from "./assets/pages/Homepage";
-import PageNotFound from "./assets/pages/PageNotFound";
-import Pricing from "./assets/pages/Pricing";
-import Product from "./assets/pages/Product";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import Login from "./pages/Login";
+import PageNotFound from "./pages/PageNotFound";
+import Pricing from "./pages/Pricing";
+import AppLayout from "./pages/AppLayout";
+import Product from "./pages/Product";
+import CityList from "./components/CityList";
+import { useState } from "react";
+import { useEffect } from "react";
 
-function App (){
+const BASE_URL='http://localhost:8000';
+
+function App() {
+  const [cities,setCities]=useState([]);
+  const [isLoading,setIsLoading]=useState(false);
+
+
+useEffect(function(){
+ 
+  async function fetchCities(){
+    try{
+      setIsLoading(true);
+      const res=await fetch(`${BASE_URL}/cities`);
+      const data=await res.json();
+      setCities(data)
+    }catch {
+      console.log('There was an error loading data...');
+    }finally{
+      setIsLoading(false);
+    }
+  }
   
+  fetchCities();
+},[])
+
   return (
-  <BrowserRouter>
-  <Routes>
-    <Route path='/' element={<Homepage/>}/>
-    <Route path="product" element={<Product/>}/>
-    <Route path="pricing" element={<Pricing/>}/>
-    <Route path ='*' element={<PageNotFound/>}/>
-  </Routes>
-  </BrowserRouter>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="app" element={<AppLayout />}>
+          <Route index element={<CityList cities={cities} isLoading={isLoading}/>} />
+          <Route path="cities" element={<CityList cities={cities} isLoading={isLoading}/>} />
+          <Route path="countries" element={<p>Countries</p>} />
+          <Route path="form" element={<p>Form</p>} />
+        </Route>
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 export default App;
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,17 @@
+import { Outlet } from "react-router-dom";
+import Logo from "../pages/Logo";
+import AppNav from "./AppNav";
+import Footer from "./Footer";
+import styles from "./Sidebar.module.css";
+
+export default function Sidebar() {
+  return (
+    <div className={styles.sidebar}>
+      <Logo />
+      <AppNav/>
+
+      <Outlet/>
+    <Footer/>
+    </div>
+  );
+}
